f543bc00fef2e7d4627c9d2535027e337239a39a
Raine Edrosa <ltedrosa@gmail.com>
2014-10-24 19:47:01 +0800

    fixed add, remove and authentication

--- encryption_attempt/encryption_attempt.ino
+++ encryption_attempt/encryption_attempt.ino
@@ -1,8 +1,9 @@
 #include <MD5.h>
-char *question = "";
-char *key = "12345";
-String devices[3] = {"Raine", "", ""};
+String key = "12345";
+String main_device = "Raine";
+String devices[3] = {"", "", ""};
 unsigned long time;
+String str;
 
 void setup() {
   Serial.begin(9600);
@@ -11,67 +12,113 @@ void setup() {
 
 void loop()
 {
-  String string = "";
+  str = "";
   if(Serial.available())
   {
     delay(100);
     while (Serial.available()) {
-      string = string + char(Serial.read());
+      str = str + char(Serial.read());
     }
   }
-  
-  if (string == "request") {
+
+  if (str == "request") {
     time = millis();
     Serial.println(time); //question
-  } else {
-    if (string != "") {
-      if (string.substring(0,10) == "add_device") {
-        boolean check = match(string.substring(11,43));
-        Serial.println("add device");
-        delay(100);
-    
-        if (check) {
-          for (int i=0; i<5; i++)  {
-            if (devices[i] == "") {
-              String device = string.substring(44);
-              //device.toCharArray(devices[i], sizeof(device));
-              devices[i] = device;
-              Serial.println("ok");
-              //return;
-            }
-          }
-          //Serial.println("Devices full");
-        } else {
-          Serial.println("Unauthorized!"); 
-        }
-      } else {
-        boolean check = match(string);
+  } 
+  else {
+    if (str != "") {
+      if (str.length() > 44 && str.substring(0,10) == "add_device") {
+        String result = addRemove("add");
+        Serial.println(result);
+        time = 0;
+      }
+      else if (str.length() > 46 && str.substring(0,13) == "remove_device") {
+        String result = addRemove("remove");
+        Serial.println(result);
+        time = 0;
+      }
+      else {
+        boolean check = match(main_device, str);
         if (check) {
           Serial.println("Match!");
+          time = 0;
           //rotate
-        } else {
-          Serial.println("Invalid!");
+        } 
+        else {
+          for (int i=0; i<3; i++) {
+            check = match(devices[i], str);
+            if (check) {
+              Serial.println("Match!");
+              time = 0;
+              //rotate
+              break;
+            }
+          }
+          if (!check) {
+            Serial.println("Invalid!");
+          }
         }
+        time = 0;
       }
     }
   }
 }
 
-boolean match(String check) {
-  String temp = String(time);
-  temp.toCharArray(question, sizeof(temp));
-  for (int i=0; i<3; i++) {
-    char *raw = strcat(key,question);
-    String final = raw + devices[i];
-    int length = final.length()+1;
-    char *make = (char *)malloc(length);
-    final.toCharArray(make, length);
-    unsigned char* hash = MD5::make_hash(make);
-    String md5str = MD5::make_digest(hash, 16);
-    
-    if (md5str.equals(check)) {
-      return true;
-    }
+boolean match(String device, String check) {
+  String question = String(time);
+  String final = key + question + device;
+  Serial.println(final);
+  char *make = makeChar(final);
+  unsigned char* hash = MD5::make_hash(make);
+  char *md5str = MD5::make_digest(hash, 16);
+  free(make);
+
+  String md = String(md5str);
+  Serial.println(md);
+  if (md.equals(check)) {
+    return true;
   }
   return false;
 }
+
+char *makeChar(String a) {
+  int length = a.length()+1;
+  char *result = (char *)malloc(length);
+  a.toCharArray(result, length);
+  return result;
+}
+
+String addRemove(String type) {
+  boolean check = false;
+  if (type == "add") {
+    check = match(main_device, str.substring(11,43));
+  } 
+  else {
+    check = match(main_device, str.substring(14,46));
+  }
+  if (!check) {
+    return "Unauthorized.";
+  } else {
+    for (int i=0; i<3; i++)  {
+      if (type == "add") {
+        if (devices[i].length() == 0) {
+          String device = str.substring(44); 
+          devices[i] = device;
+          return "Device added.";
+        }
+      } 
+      else {
+        if (devices[i] == str.substring(47)) {
+          devices[i] = "";
+          return "Device removed.";
+        }
+      }
+    }
+    if (type == "add") {
+      return "Devices full.";
+    } else {
+      return "Device not found.";
+    }
+  }
+}
+
